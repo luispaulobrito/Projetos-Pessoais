c167eab944ebde22d088e1d1eb0ed0a0b2e30f1e
luispaulobrito <luiss_paullo@hotmail.com>
2023-02-03 15:03:31 -0300

    front iniciado

--- 06 - contatos-spring-boot/contatos-app/src/app/app.module.ts
+++ 06 - contatos-spring-boot/contatos-app/src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { ContatoService } from './contato.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
